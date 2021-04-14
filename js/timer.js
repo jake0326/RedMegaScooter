9e24eb124cfea86c61c041a27abd05eca32a7213
jake0326 <38882815+jake0326@users.noreply.github.com>
2021-04-14 11:33:36 -0400

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,18 @@
+"use strict";
+	/* Run the function*/
+	runClock();
+	setInterval("runClock()", 1000);
+	/*Funciton to create and run clock*/
+	function runClock()
+	{
+	
+	/* Store the current date and time */
+	var currentDay = new Date();
+	var dateStr = currentDay.toLocaleDateString();
+	var timeStr = currentDay.toLocaleTimeString();
 
+	/* Display the current date and time */
+	document.getElementById("dateNow").innerHTML =
+	dateStr + "<br />" + timeStr;
+	
+}
